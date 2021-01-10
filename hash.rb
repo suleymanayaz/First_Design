9576a757aa9ebc26966ea8fd1163df0f0e695958
Suleyman AYAZ <45987676+suleymanayaz@users.noreply.github.com>
2021-01-10 17:46:08 +0300

    Comment Update

--- hash.rb
+++ hash.rb
@@ -1,6 +1,5 @@
 metin = File.read("summary.txt")                                                                                                                                                                                                                                                                        
-#File.read parantez icerisindeki txt dosyasi hash.rb ile ayni yerde bulundugu icin sadece "summary.txt" yazmak yeterli dir.
-#Eger aynı yer degilse "C:\Users\....\...." gibi dosya konumunu belirterek de metin dosyasini okutabilirsiniz.
+# Don't forget to give file path File.read("C:\Users\........\summary.txt")
 a = {}
 metin.split.each do |kelime|
     kelime.delete!".,)("
